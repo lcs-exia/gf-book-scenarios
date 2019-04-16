02978be5c1108769ac1df8da2bbaabea3bbb998a
Eric Xia <exia@lcs.on.ca>
2019-04-16 13:17:33 -0400

    Declared two arrays to track keyboard keys and sound for piano white keys

--- chapter06/piano-1/Key.java
+++ chapter06/piano-1/Key.java
@@ -7,14 +7,21 @@ public class Key extends Actor
      * These can be used anywhere in the class below.
      */
     private boolean keyAlreadyDown;
+    // Will contain the key and sound file thatthis instance of the class will play
+    private String key;
+    private String sound;
     
     /**
      * Create a new key.
      */
-    public Key()
+    public Key(String keyName, String soundFile)
     {
         // No key has been pressed
         keyAlreadyDown = false;
+        
+        // Set the instance variab;es using the paramteres
+        key = keyName;
+        sound = soundFile;
     }
 
     /**
@@ -25,7 +32,7 @@ public class Key extends Actor
         // Animate the piano key being pressed
         // Condition 1 - is the "g" key being pressed
         // Condition 2 - was NOT already down
-        if (((Greenfoot.isKeyDown("g")) && !keyAlreadyDown))
+        if (((Greenfoot.isKeyDown(key)) && !keyAlreadyDown))
         {
             // Key is down
             setImage("white-key-down.png");
@@ -35,7 +42,7 @@ public class Key extends Actor
         // Stop showing the key being pressed
         // Condition 1 - The key was down the last act() fired
         // Condition 2 - The "g" has been released (no longer being pressed)
-        if ( (keyAlreadyDown == true) && (!Greenfoot.isKeyDown ("g"))) 
+        if ( (keyAlreadyDown == true) && (!Greenfoot.isKeyDown (key))) 
         {
             // Key is up
             setImage("white-key.png");
@@ -48,7 +55,7 @@ public class Key extends Actor
      */
     public void play()
     {
-        Greenfoot.playSound("3a.wav");
+        Greenfoot.playSound(sound + ".wav");
     }
 }
 
