2f4215331105c1f2c66a5dd05b69d38e3c05cce9
Eric Xia <exia@lcs.on.ca>
2019-04-26 13:57:14 -0400

    remove leaf when block touches

--- chapter07/autumn-1/Block.java
+++ chapter07/autumn-1/Block.java
@@ -19,6 +19,31 @@ public class Block extends Actor
         move();
         checkEdge();
         checkMouseClick();
+        checkForLeafCollision();
+    }
+    
+    /**
+     * Remove a Leaf object if we are intersecting with it
+     */
+    private void checkForLeafCollision()
+    {
+        // Get an object reference to a single Leaf from any Leaf objects we
+        // are currently intersecting
+        // NOTE: By placing a type in brackets before the call to 
+        // getOneIntersectingObject
+        // we ***cast*** (or force) the type to change from Actor to Leaf
+        // NOTE 2: We can do this because Leaf is a subclass of Actor
+        Leaf someLeaf = (Leaf) getOneIntersectingObject(Leaf.class);
+        
+        // Check that the someLeaf object actually has a reference to an object
+        // If it is null, that eans there was no leaf intersecting the Block 
+        // this time around
+        if (someLeaf != null)
+        {
+            // someLeaf is NOT null... so now we can remove it
+            World MyWorld = getWorld();
+            MyWorld.removeObject(someLeaf);
+        }
     }
     
     /**
@@ -42,8 +67,9 @@ public class Block extends Actor
             // Add a new Leaf object to the world
             // 1. Add a reference to the World
             World myWorld = getWorld();
-            // 2. Use the object reference to call, or use, a method on the World object
-            myWorld.addObject(new Leaf(), x, y);
+            // 2. Use the object reference to call, or use, a method on the 
+            // World object
+            myWorld.addObject(new Leaf(), x + 50, y + 50);
         }
     }
     
